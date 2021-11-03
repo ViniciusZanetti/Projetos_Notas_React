1e31ef33c637e1646b7356d1ccddff79a0c48a0e
ViniciusZanetti <58536337+ViniciusZanetti@users.noreply.github.com>
2021-11-03 16:55:21 -0300

    notas sendo criadas mas não são exibidas

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
-import ListaDeNotas from "./Componetes/ListaDeNotas";
-import FormularioCadastro from "./Componetes/FormularioCadastro";
+import ListaDeNotas from "./Componetes/ListaDeNotas/ListaDeNotas";
+import FormularioCadastro from "./Componetes/FormularioCadastro/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
 
-class App extends Component() {
-  render(){
+class App extends Component {
+
+  constructor(){
+    super();
+    this.notas = []
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = {
+      titulo: titulo,
+      texto: texto
+    }
+
+    this.notas.push(novaNota)
+    console.log(this.notas.length)
+  }
+  render() {
     return (
-      <div>
-        <FormularioCadastro />
-        <ListaDeNotas />
-      </div>
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.notas}/>
+      </section>
     );
   }
 }
---
+++ ceep/src/Componetes/ListaDeNotas/ListaDeNotas.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import CardNota from "../CardNotas/CardNotas";
+import "./style.css";
+
+
+class ListaDeNotas extends Component {
+  
+  render() {
+    return (
+      <ul className="lista-notas">
+        {this.props.notas.map((nota, index) => {
+          return (
+            <li className="lista-notas_item" key={index}>
+              <CardNota />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default ListaDeNotas;
